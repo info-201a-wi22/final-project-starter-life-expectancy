dec81e0bd09ca76b279069576f048f930400e3de
lizethvl <lizethvl@uw.edu>
2022-02-16 15:55:28 -0800

    Finished Summary Info and Table

--- source/summary_table.R
+++ source/summary_table.R
@@ -11,7 +11,7 @@ source("summary_info.R")
 # Create table of summary information
 sum_table <- data.frame(summary_info)
 
-
+# Group all data from the same country into the average of the different years
 sum_table <- sum_table %>%
   group_by(country) %>%
   summarize(vaccination_percentage = mean(vaccination_percentage, na.rm = TRUE), 
@@ -22,20 +22,20 @@ sum_table <- sum_table %>%
             health_expenditure = mean(health_expenditure, na.rm = TRUE), 
             education_ratio = mean(education_ratio, na.rm = TRUE))
 
+# Take all Inf values and remove them
 is.na(sum_table) <- sapply(sum_table, is.infinite)
 sum_table <- na.omit(sum_table)
 
+# Round numbers to two decimal places and remove scientific notation
 sum_table <- sum_table %>%
   mutate_if(is.numeric,
-            format,
-            scientific = FALSE) %>%
-  mutate_if(is.numeric,
             round,
-            digits = 2)
+            digits = 2) %>%
+  mutate_if(is.numeric,
+            format,
+            scientific = FALSE)
 
-sum_table$total_vaccination_hep <- round(total_vaccination_hep)
-
-
+# Rename column names
 sum_table <- sum_table %>%
   rename("Country" = country, 
          "Average Vaccination Percentage" = vaccination_percentage, 
@@ -46,4 +46,3 @@ sum_table <- sum_table %>%
          "Expenditure on Health from GDP in USD" = health_expenditure, 
          "Percentage of Schooling Years vs Life Expectancy" = education_ratio)
 
-View(sum_table)
