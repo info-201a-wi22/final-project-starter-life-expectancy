0b07a22691e53408ece6e0948d47298b7a1462c5
kaymtoun <kaymtoun@uw.edu>
2022-02-16 11:09:41 -0800

    Updated Source File

---
+++ source/Testing_Life Expectancy.R
@@ -0,0 +1,16 @@
+# INFO 201: Project Pt 2
+# File 2 
+# Alcohol and mortality 
+# Scatterplot 
+
+#Loading Packages 
+#library(ggplot2)
+#library(tindyverse)
+
+#set working directory 
+setwd("~/info201_code/final-project-starter-life-expectancy/source")
+
+#Load data 
+life_expectancy <- read.csv("LifeExpectancyData.csv")
+
+#Scatterplot 
